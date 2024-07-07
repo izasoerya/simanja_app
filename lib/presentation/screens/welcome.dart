146dd971de8b257dbfe652202662106a607f40b9
izasoerya <ihza.surya@gmail.com>
2024-07-08 01:03:53 +0700

    refactor folder

--- lib/presentation/screens/welcome.dart
+++ lib/presentation/screens/welcome.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'Welcome to the app!',
-              style: TextStyle(fontFamily: 'Gilroy', fontSize: 24),
+              style: GoogleFonts.openSans(fontSize: 24),
             ),
           ],
         ),
