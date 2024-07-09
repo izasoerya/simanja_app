697fd6c3a63d19344dbc73449158f1723c3273cd
izasoerya <ihza.surya@gmail.com>
2024-07-10 00:15:28 +0700

    chore: dashboard kader

---
+++ lib/presentation/widgets/organism/dashboard_remaja_drafted.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:simanja_app/presentation/theme/global_theme.dart';
+import 'package:simanja_app/presentation/widgets/atom/attentance_bpjs.dart';
+
+class DashboardRemajaDrafted extends StatelessWidget {
+  const DashboardRemajaDrafted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: MediaQuery.of(context).size.height * 0.25,
+      decoration: BoxDecoration(
+        color: GlobalTheme().primaryColor,
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(
+            child: Column(
+              children: [
+                Text(
+                  'Jumlah Remaja Terdaftar SiMandja',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '//TODO: Posyandu Remaja Pramahayu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Text(
+            '//TODO: 4',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/widgets/template/dashboard_content.dart
+++ lib/presentation/widgets/template/dashboard_content.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:simanja_app/presentation/widgets/organism/dashboard_count_remaja.dart';
+import 'package:simanja_app/presentation/widgets/organism/dashboard_remaja_drafted.dart';
 
 class DashboardContent extends StatelessWidget {
   const DashboardContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [DashboardCountRemaja()],
+    return const Column(
+      children: [
+        DashboardCountRemaja(),
+        Padding(padding: EdgeInsets.only(top: 5)),
+        DashboardRemajaDrafted(),
+      ],
     );
   }
 }
