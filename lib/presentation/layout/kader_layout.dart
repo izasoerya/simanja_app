395e4df74be15d36bff641572bec5b51c66c7a33
izasoerya <ihza.surya@gmail.com>
2024-07-11 21:42:16 +0700

    chore: change private var to provider

--- lib/presentation/layout/kader_layout.dart
+++ lib/presentation/layout/kader_layout.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simanja_app/presentation/provider/provider_index_page.dart';
-import 'package:simanja_app/presentation/provider/provider_user.dart';
 import 'package:simanja_app/presentation/router/router.dart';
 import 'package:simanja_app/presentation/theme/global_theme.dart';
 
