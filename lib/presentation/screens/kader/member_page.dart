6656bba33eaf2ca1d8c6d2725599b24b6f032af0
izasoerya <ihza.surya@gmail.com>
2024-07-16 12:56:23 +0700

    chore: rebase routing

--- lib/presentation/screens/kader/member_page.dart
+++ lib/presentation/screens/kader/member_page.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:simanja_app/presentation/provider/provider_index_page.dart';
 import 'package:simanja_app/presentation/widgets/atom/custom_searchbar.dart';
 import 'package:simanja_app/presentation/widgets/organism/listview_account.dart';
 import 'package:simanja_app/presentation/widgets/template/title_w_posyandu.dart';
 
-class MemberPage extends StatelessWidget {
+class MemberPage extends ConsumerWidget {
   const MemberPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        TitleWPosyandu(),
-        Padding(padding: EdgeInsets.only(top: 30)),
-        CustomSearchBar(label: 'Ihza Surya P...', icon: Icons.search),
-        Padding(padding: EdgeInsets.only(top: 30)),
-        ListViewAccount(),
-      ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return PopScope(
+      onPopInvoked: (didPop) => changePageIndex(ref, 0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TitleWPosyandu(),
+          Padding(padding: EdgeInsets.only(top: 30)),
+          CustomSearchBar(label: 'Ihza Surya P...', icon: Icons.search),
+          Padding(padding: EdgeInsets.only(top: 30)),
+          ListViewAccount(),
+        ],
+      ),
     );
   }
 }
