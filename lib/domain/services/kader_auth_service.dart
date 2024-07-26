2705efeff8b0bfa486813646b952687c5daa53bb
izasoerya <ihza.surya@gmail.com>
2024-07-27 01:15:43 +0700

    feat: Add getPosyanduList method to KaderAuthentication service

--- lib/domain/services/kader_auth_service.dart
+++ lib/domain/services/kader_auth_service.dart
@@ -16,6 +16,11 @@ class KaderAuthentication {
     return user.copyWith(uid: 'dummy'); // return invalid account
   }
 
+  Future<List<String>> getPosyanduList() async {
+    final response = await KaderAuthImplementation().getUsers();
+    return response.map((e) => e.namePosyandu).toList();
+  }
+
   Future<void> debugUsers() async {
     var data = await KaderAuthImplementation().getUsers();
     for (var user in data) {
