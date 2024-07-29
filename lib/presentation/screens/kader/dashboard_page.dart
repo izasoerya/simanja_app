4b999877bf66d98534a6371eb6eec876d1769788
izasoerya <ihza.surya@gmail.com>
2024-07-30 03:23:25 +0700

    Refactor DashboardKader to remove unused imports and optimize code structure

--- lib/presentation/screens/kader/dashboard_page.dart
+++ lib/presentation/screens/kader/dashboard_page.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simanja_app/presentation/widgets/atom/template_title.dart';
 import 'package:simanja_app/presentation/widgets/organism/appbar_content.dart';
 import 'package:simanja_app/presentation/widgets/template/activity_list.dart';
 import 'package:simanja_app/presentation/widgets/template/dashboard_content.dart';
 import 'package:simanja_app/presentation/widgets/template/summary_remaja.dart';
 
-class DashboardKader extends ConsumerWidget {
+class DashboardKader extends StatelessWidget {
   const DashboardKader({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Center(
       child: SingleChildScrollView(
         child: Column(
