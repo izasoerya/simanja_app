f1d0bc1dfa0eb9d9549016a120a734eae076f7df
izasoerya <ihza.surya@gmail.com>
2024-07-31 16:10:23 +0700

    feat: implement process for get data by remaja uid

--- lib/domain/repositories/remaja_health_repo.dart
+++ lib/domain/repositories/remaja_health_repo.dart
@@ -2,7 +2,7 @@ import 'package:simanja_app/domain/entities/remaja_health.dart';
 
 abstract class RemajaHealthRepo {
   Future<List<HealthPropertiesRemaja>> getHealths();
-  Future<HealthPropertiesRemaja> getHealthByUID(String uid);
+  Future<HealthPropertiesRemaja?> getHealthByUID(String uid);
 
   Future<HealthPropertiesRemaja?> createHealth(HealthPropertiesRemaja health);
   Future<HealthPropertiesRemaja?> updateHealth(HealthPropertiesRemaja health);
--- lib/infrastructure/iremaja_health.dart
+++ lib/infrastructure/iremaja_health.dart
@@ -1,5 +1,6 @@
 import 'package:simanja_app/domain/entities/remaja_health.dart';
 import 'package:simanja_app/domain/repositories/remaja_health_repo.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class RemajaHealthImplementation implements RemajaHealthRepo {
   @override
@@ -15,9 +16,19 @@ class RemajaHealthImplementation implements RemajaHealthRepo {
   }
 
   @override
-  Future<HealthPropertiesRemaja> getHealthByUID(String uid) {
-    // TODO: implement getHealthByUID
-    throw UnimplementedError();
+  Future<HealthPropertiesRemaja?> getHealthByUID(String uid) async {
+    try {
+      final response = await Supabase.instance.client
+          .from('health')
+          .select()
+          .eq('uid', uid)
+          .single();
+      HealthPropertiesRemaja data = HealthPropertiesRemaja.fromJSON(response);
+      return data;
+    } catch (e) {
+      print('Error:$e');
+      return null;
+    }
   }
 
   @override
