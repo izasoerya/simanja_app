ef687fea6c592e910a33d9e9025f8c2f13350ec7
izasoerya <ihza.surya@gmail.com>
2024-08-03 00:59:43 +0700

    allow null data

--- lib/domain/entities/remaja_health.dart
+++ lib/domain/entities/remaja_health.dart
@@ -1,42 +1,42 @@
 class HealthPropertiesRemaja {
-  final String uid;
-  final String uidRemaja;
-  final double weight;
-  final double height;
-  final double armCircumference;
-  final double abdominalCircumference;
-  final double bloodPressure;
-  final double hemoglobin;
-  final double cholesterol;
-  final double bloodSugar;
-  final double tds;
-  final double tdd;
-  final DateTime checkedAt;
-  final bool kek;
-  final bool anemia;
-  final bool smoker;
-  final bool tablet;
-  final String note;
+  final String? uid;
+  final String? uidRemaja;
+  final double? weight;
+  final double? height;
+  final double? armCircumference;
+  final double? abdominalCircumference;
+  final double? bloodPressure;
+  final double? hemoglobin;
+  final double? cholesterol;
+  final double? bloodSugar;
+  final double? tds;
+  final double? tdd;
+  final DateTime? checkedAt;
+  final bool? kek;
+  final bool? anemia;
+  final bool? smoker;
+  final bool? tablet;
+  final String? note;
 
   HealthPropertiesRemaja({
-    required this.uid,
-    required this.uidRemaja,
-    required this.weight,
-    required this.height,
-    required this.armCircumference,
-    required this.abdominalCircumference,
-    required this.bloodPressure,
-    required this.hemoglobin,
-    required this.cholesterol,
-    required this.bloodSugar,
-    required this.tds,
-    required this.tdd,
-    required this.checkedAt,
-    required this.kek,
-    required this.anemia,
-    required this.smoker,
-    required this.tablet,
-    required this.note,
+    this.uid,
+    this.uidRemaja,
+    this.weight,
+    this.height,
+    this.armCircumference,
+    this.abdominalCircumference,
+    this.bloodPressure,
+    this.hemoglobin,
+    this.cholesterol,
+    this.bloodSugar,
+    this.tds,
+    this.tdd,
+    this.checkedAt,
+    this.kek,
+    this.anemia,
+    this.smoker,
+    this.tablet,
+    this.note,
   });
 
   HealthPropertiesRemaja copyWith({
@@ -118,7 +118,7 @@ class HealthPropertiesRemaja {
       'blood_sugar': bloodSugar,
       'tds': tds,
       'tdd': tdd,
-      'checked_at': checkedAt.toIso8601String(),
+      'checked_at': checkedAt!.toIso8601String(),
       'status_kek': kek,
       'status_anemia': anemia,
       'status_smoker': smoker,
