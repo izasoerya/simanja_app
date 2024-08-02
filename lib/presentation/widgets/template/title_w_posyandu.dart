ad1d02f032bc616a05893cfc359109da15ba973f
izasoerya <ihza.surya@gmail.com>
2024-08-02 20:27:30 +0700

    feat: Remove unnecessary code in TitleWPosyandu widget

--- lib/presentation/widgets/template/title_w_posyandu.dart
+++ lib/presentation/widgets/template/title_w_posyandu.dart
@@ -1,35 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simanja_app/presentation/provider/provider_user.dart';
-import 'package:simanja_app/presentation/router/router.dart';
 import 'package:simanja_app/presentation/theme/global_theme.dart';
-import 'package:simanja_app/presentation/widgets/atom/goback_icon.dart';
 import 'package:simanja_app/presentation/widgets/atom/template_title.dart';
 
 class TitleWPosyandu extends ConsumerWidget {
   final String title;
   const TitleWPosyandu({super.key, required this.title});
 
-  void onTap() => router.pop();
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
-        Container(
-          height: 50,
-          width: MediaQuery.of(context).size.width,
-          alignment: Alignment.centerLeft,
-          child: BackButtonAppbar(
-            onTap: onTap,
-          ),
-        ),
         TemplateTitle(text: title),
         Text(ref.watch(userKaderProvider).namePosyandu,
             style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
-                color: GlobalTheme().secondaryColor)),
+                color: const GlobalTheme().secondaryColor)),
         const Padding(padding: EdgeInsets.only(top: 5)),
         Container(
           height: 2,
