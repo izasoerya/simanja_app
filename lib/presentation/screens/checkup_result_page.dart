cec830e662e8c2d92f90631d100bc9bcb45c1dff
izasoerya <ihza.surya@gmail.com>
2024-08-05 21:14:34 +0700

    Refactor checkup_result.dart and checkup_result_page.dart for better code organization and readability

---
+++ lib/presentation/screens/checkup_result_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:simanja_app/domain/entities/remaja_auth.dart';
+import 'package:simanja_app/domain/entities/remaja_health.dart';
+import 'package:simanja_app/domain/services/remaja_auth_service.dart';
+import 'package:simanja_app/domain/services/remaja_heath_service.dart';
+import 'package:simanja_app/presentation/widgets/atom/user_identity.dart';
+import 'package:simanja_app/presentation/widgets/template/checkup_result_field.dart';
+import 'package:simanja_app/presentation/widgets/template/title_w_posyandu.dart';
+
+class CheckupResultPage extends StatelessWidget {
+  final String healthUID;
+  const CheckupResultPage({super.key, required this.healthUID});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.wait([
+        RemajaHealthService().getRemajaHealthbyUID(healthUID),
+        RemajaHealthService().getRemajaHealthbyUID(healthUID).then(
+              (healthData) =>
+                  RemajaAuthentication().getUserbyUID(healthData!.uid!),
+            ),
+      ]),
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasData) {
+          final HealthPropertiesRemaja healthData = snapshot.data![0];
+          final UserRemaja userData = snapshot.data![1];
+          return Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            margin: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const TitleWPosyandu(title: 'HASIL\nPENIMBANGAN'),
+                  UserIdentity(user: userData),
+                  CheckupResultField(item: healthData),
+                ],
+              ),
+            ),
+          );
+        }
+        return const Center(
+          child: Text('Data tidak ditemukan',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/widgets/template/checkup_result_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:simanja_app/domain/entities/remaja_health.dart';
+
+class CheckupResultField extends StatelessWidget {
+  final HealthPropertiesRemaja? item;
+  const CheckupResultField({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
