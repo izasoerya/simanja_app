9f358a483b801bbdf7bc1c9e959402c19cf37c62
izasoerya <ihza.surya@gmail.com>
2024-08-08 01:22:24 +0700

    feat: Add padding to activity page for better spacing

--- lib/presentation/screens/kader/activity_page.dart
+++ lib/presentation/screens/kader/activity_page.dart
@@ -79,6 +79,7 @@ class _ActivityPageState extends ConsumerState<ActivityPage> {
                     }
                   },
                 ),
+                const Padding(padding: EdgeInsets.only(top: 50)),
               ],
             ),
           ),
