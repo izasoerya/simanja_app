f858ed67b7724197019b83bae57b3adfeace9fe6
izasoerya <ihza.surya@gmail.com>
2024-08-14 10:11:44 +0700

    refactor: Adjust padding and margin in ImageBox widget

--- lib/presentation/widgets/atom/image_box.dart
+++ lib/presentation/widgets/atom/image_box.dart
@@ -26,9 +26,10 @@ class ImageBox extends StatelessWidget {
           width: width ?? 100,
           height: height ?? 120,
           padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(5),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: color ?? Colors.grey.shade200,
+            color: color ?? Colors.grey.shade400,
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
