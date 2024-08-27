faec62686996858680d90ccbb8ff5daab3158bd7
izasoerya <ihza.surya@gmail.com>
2024-08-27 13:52:25 +0700

    feat: Add UGM logo to welcome screen

--- lib/presentation/screens/welcome.dart
+++ lib/presentation/screens/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simanja_app/presentation/router/router.dart';
 import 'package:simanja_app/presentation/theme/global_theme.dart';
+import 'package:simanja_app/presentation/theme/global_theme.dart';
 import 'package:simanja_app/presentation/widgets/atom/submit_button.dart';
 import 'package:sizer/sizer.dart';
 
@@ -20,12 +21,43 @@ class WelcomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text(
-                'Selamat \nDatang',
-                textAlign: TextAlign.center, // Center text
-                style: const GlobalTheme()
-                    .headerStyle
-                    .copyWith(fontSize: 24.sp, fontWeight: FontWeight.w900),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: const AssetImage('assets/logo/ugm.png'),
+                        height: 5.h,
+                        width: 10.w,
+                      ),
+                      SizedBox(width: 4.w),
+                      Text(
+                        'Departemen Layanan dan\nInformasi Kesehatan',
+                        style: const GlobalTheme().paragraphStyle.copyWith(
+                            fontSize: 10.sp,
+                            fontWeight: FontWeight.bold,
+                            color: const GlobalTheme().primaryColor),
+                        overflow: TextOverflow.visible,
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 3.h)),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Text(
+                      'Aplikasi ini Digunakan Sebagai Sistem Informasi Pencatatan dan Manajemen Data Kesehatan Remaja yang Dilakukan di Posyandu Remaja',
+                      style: const GlobalTheme().paragraphStyle.copyWith(
+                            fontSize: SizerUtil.deviceType == DeviceType.tablet
+                                ? 6.sp
+                                : 8.sp,
+                            fontWeight: FontWeight.bold,
+                            color: GlobalTheme().primaryColor,
+                          ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               ),
               Image(
                 image: const AssetImage('assets/logo/SiMandja-Logo.png'),
