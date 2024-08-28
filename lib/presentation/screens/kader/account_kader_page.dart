ae47c2614d7f05cc929ee1668380de0b37341de4
izasoerya <ihza.surya@gmail.com>
2024-08-28 19:21:44 +0700

    refactor: Update account page layout and functionality

--- lib/presentation/screens/kader/account_kader_page.dart
+++ lib/presentation/screens/kader/account_kader_page.dart
@@ -15,35 +15,31 @@ class AccountKaderPage extends ConsumerWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return PopScope(
-      onPopInvoked: (didPop) {
-        router.go('/login-kader/dashboard-kader');
-        ref.watch(pageIndexProvider.notifier).state = 0;
-      },
-      child: Container(
-        width: screenWidth,
-        margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-        alignment: Alignment.topCenter,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: screenHeight * 0.05)),
-              ProfilPicture(kader: kaderAccount, isSelf: true),
-              SizedBox(height: screenHeight * 0.02),
-              SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: DetailAccount(kader: kaderAccount)),
-              SizedBox(height: screenHeight * 0.05),
-              SubmitButton(
-                text: 'Keluar Akun',
-                onClick: () {
-                  ref.watch(pageIndexProvider.notifier).state = 0;
+    return Container(
+      width: screenWidth,
+      margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+      alignment: Alignment.topCenter,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: screenHeight * 0.05)),
+            ProfilPicture(kader: kaderAccount, isSelf: true),
+            SizedBox(height: screenHeight * 0.02),
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: DetailAccount(kader: kaderAccount)),
+            SizedBox(height: screenHeight * 0.05),
+            SubmitButton(
+              text: 'Keluar Akun',
+              onClick: () {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  ref.read(pageIndexProvider.notifier).state = 0;
                   router.go('/');
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: screenHeight * 0.05)),
-            ],
-          ),
+                });
+              },
+            ),
+            Padding(padding: EdgeInsets.only(top: screenHeight * 0.05)),
+          ],
         ),
       ),
     );
